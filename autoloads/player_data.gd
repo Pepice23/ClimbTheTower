0077421cddc30ba1830fae82abb1e3f9a079c554
Molnár József <mojo12984@gmail.com>
2024-07-02 08:12:30 +0200

    add playerdata autoload

---
+++ autoloads/player_data.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var floor_progress = 1
+var enemy_progress = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
