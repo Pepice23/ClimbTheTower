26563ce5508694382568719cd0164dec0d616e6a
Molnár József <mojo12984@gmail.com>
2024-07-04 11:21:01 +0200

    removed stage testing buttons

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -6,15 +6,6 @@ extends Node2D
 func _ready():
 	pass
 
-func _on_increase_enemy_pressed():
-	PlayerData.increase_current_enemy()
-
-func _on_reset_enemy_pressed():
-	PlayerData.reset_current_enemy()
-
-func _on_inc_floor_res_enemy_pressed():
-	PlayerData.increase_current_floor()
-
 func _on_attack_timer_timeout():
 	animation_player.play("weapon_attack")
 
@@ -23,3 +14,9 @@ func _on_attack_button_pressed():
 
 func _on_add_xp_pressed():
 	PlayerData.increase_current_xp(80)
+
+func _on_add_damage_pressed():
+	PlayerData.increase_current_damage()
+
+func _on_add_gold_pressed():
+	PlayerData.increase_current_gold()
