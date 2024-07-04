f0e331fe4724d714fe5898d715e4075577b10660
Molnár József <mojo12984@gmail.com>
2024-07-04 08:59:15 +0200

    set boss_timer progress bar

--- autoloads/enemy_data.gd
+++ autoloads/enemy_data.gd
@@ -2,6 +2,8 @@ extends Node
 
 var enemy_current_hp = 400
 var enemy_max_hp = 400
+var boss_time_max = 30
+var boss_time_current = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- scenes/enemy_stat_display/enemy_stat_display.gd
+++ scenes/enemy_stat_display/enemy_stat_display.gd
@@ -4,10 +4,13 @@ extends VBoxContainer
 @onready var hp_progress_bar = $HPProgressBar
 @onready var hp_progress_bar_text = $HPProgressBar/HPProgressBarLabel
 @onready var boss_timer_container = $BossTimerContainer
+@onready var boss_timer_progress_bar = $BossTimerContainer/BossTimerProgressBar
+@onready var boss_timer_progress_bar_text = $BossTimerContainer/BossTimerProgressBar/BossTimerLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_defaults()
+	set_boss_defaults()
 	PlayerData.connect("show_boss_timer", toggle_boss_timer_container_visibility)
 	PlayerData.connect("change_enemy_level", level_update)
 	PlayerData.connect("change_enemy_health", update_hp_progress)
@@ -22,9 +25,12 @@ func level_update():
 	level_text.text = str(PlayerData.current_floor)
 
 func set_progress_fill_color():
-	var style_box = StyleBoxFlat.new()
-	style_box.bg_color = Color.RED
-	hp_progress_bar.add_theme_stylebox_override("fill", style_box)
+	var style_box_hp = StyleBoxFlat.new()
+	var style_box_boss_timer = StyleBoxFlat.new()
+	style_box_hp.bg_color = Color.RED
+	style_box_boss_timer.bg_color = Color.YELLOW_GREEN
+	hp_progress_bar.add_theme_stylebox_override("fill", style_box_hp)
+	boss_timer_progress_bar.add_theme_stylebox_override("fill", style_box_boss_timer)
 
 func set_defaults():
 	set_progress_fill_color()
@@ -35,3 +41,12 @@ func set_defaults():
 func update_hp_progress():
 	hp_progress_bar.value = EnemyData.enemy_current_hp
 	hp_progress_bar_text.text = "HP: %d / %d" % [EnemyData.enemy_current_hp, EnemyData.enemy_max_hp]
+
+func set_boss_defaults():
+	boss_timer_progress_bar.value = EnemyData.boss_time_current
+	boss_timer_progress_bar.max_value = EnemyData.boss_time_max
+	update_boss_timer_progress()
+
+func update_boss_timer_progress():
+	boss_timer_progress_bar.value = EnemyData.boss_time_current
+	boss_timer_progress_bar_text.text = "%d seconds left" % [EnemyData.boss_time_current]
