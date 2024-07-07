4b245364fbab508b044ac032dd051d68a6212745
Molnár József <mojo12984@gmail.com>
2024-07-07 16:45:01 +0200

    added new formula to calculate enemy health
    needs more work

--- autoloads/enemy_data.gd
+++ autoloads/enemy_data.gd
@@ -4,6 +4,7 @@ var enemy_current_hp = 400
 var enemy_max_hp = 400
 var boss_time_max = 30
 var boss_time_current = 30
+var base_hp = 10
 
 signal change_enemy_health
 signal change_boss_timer
@@ -13,6 +14,7 @@ func _ready():
 	pass # Replace with function body.
 
 func prepare_next_enemy():
+	enemy_max_hp = base_hp * 0.8 * pow(1.1, PlayerData.current_floor) * PlayerData.current_enemy + 1
 	enemy_current_hp = enemy_max_hp
 	emit_signal("change_enemy_health")
 
--- scenes/enemy_stat_display/enemy_stat_display.gd
+++ scenes/enemy_stat_display/enemy_stat_display.gd
@@ -9,13 +9,13 @@ extends VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_defaults()
-	set_boss_defaults()
 	PlayerData.connect("show_boss_timer", toggle_boss_timer_container_visibility)
 	PlayerData.connect("change_enemy_level", enemy_level_update)
 	EnemyData.connect("change_enemy_health", update_hp_progress)
 	EnemyData.connect("change_boss_timer", update_boss_timer_progress)
 	Battle.connect("player_auto_attack", update_hp_progress)
+	set_defaults()
+	set_boss_defaults()
 
 func toggle_boss_timer_container_visibility():
 	if PlayerData.current_enemy == PlayerData.enemy_max:
@@ -41,8 +41,9 @@ func set_defaults():
 	update_hp_progress()
 
 func update_hp_progress():
+	hp_progress_bar.max_value = EnemyData.enemy_max_hp
 	hp_progress_bar.value = EnemyData.enemy_current_hp
-	hp_progress_bar_text.text = "HP: %d / %d" % [EnemyData.enemy_current_hp, EnemyData.enemy_max_hp]
+	hp_progress_bar_text.text = "HP: %s / %s" % [Helpers.format_number(EnemyData.enemy_current_hp), Helpers.format_number(EnemyData.enemy_max_hp)]
 
 func set_boss_defaults():
 	boss_timer_progress_bar.value = EnemyData.boss_time_current
