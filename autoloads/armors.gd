4248abcf999e3cde532e67fc602a87fa76107285
Molnár József <mojo12984@gmail.com>
2024-07-19 10:19:37 +0200

    added armors autoload

---
+++ autoloads/armors.gd
@@ -0,0 +1,67 @@
+extends Node
+
+var level_10_armor = {
+	"armor_name": "Level 10 Armor",
+	"damage_multiplier": 2,
+	"armor_picture": "res://assets/armors/level10.png",
+	"price": 5000
+}
+
+var level_20_armor = {
+	"armor_name": "Level 20 Armor",
+	"damage_multiplier": 4,
+	"armor_picture": "res://assets/armors/level20.png",
+	"price": 20000
+}
+
+var level_30_armor = {
+	"armor_name": "Level 30 Armor",
+	"damage_multiplier": 6,
+	"armor_picture": "res://assets/armors/level30.png",
+	"price": 35000
+}
+
+var level_40_armor = {
+	"armor_name": "Level 40 Armor",
+	"damage_multiplier": 8,
+	"armor_picture": "res://assets/armors/level40.png",
+	"price": 50000
+}
+
+var level_50_armor = {
+	"armor_name": "Level 50 Armor",
+	"damage_multiplier": 10,
+	"armor_picture": "res://assets/armors/level50.png",
+	"price": 65000
+}
+
+var level_60_armor = {
+	"armor_name": "Level 60 Armor",
+	"damage_multiplier": 12,
+	"armor_picture": "res://assets/armors/level60.png",
+	"price": 80000
+}
+
+var level_70_armor = {
+	"armor_name": "Level 70 Armor",
+	"damage_multiplier": 14,
+	"armor_picture": "res://assets/armors/level70.png",
+	"price": 95000
+}
+
+var level_80_armor = {
+	"armor_name": "Level 80 Armor",
+	"damage_multiplier": 16,
+	"armor_picture": "res://assets/armors/level80.png",
+	"price": 110000
+}
+
+var level_90_armor = {
+	"armor_name": "Level 90 Armor",
+	"damage_multiplier": 18,
+	"armor_picture": "res://assets/armors/level90.png",
+	"price": 125000
+}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
